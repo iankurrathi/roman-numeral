df2c1bdd1834dbce9f7f13615e4c527a3b27cd1b
ankur <ankurrathi@gmail.com>
2022-09-19 19:01:11 +0100

    fixes test for number 3

--- src/main/java/com/test/RomanNumeral.java
+++ src/main/java/com/test/RomanNumeral.java
@@ -1,10 +1,16 @@
 package com.test;
 
+import java.util.Map;
+
 public class RomanNumeral {
-    public String convert(int i) {
-        if (i == 2) {
-            return "II";
-        }
-        return "I";
+
+    private static final Map<Integer, String> romanNumeralsMap = Map.of(
+            1, "I",
+            2, "II",
+            3, "III");
+
+    public String convert(int number) {
+        return romanNumeralsMap.get(number);
     }
+
 }
