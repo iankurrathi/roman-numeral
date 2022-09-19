59f8240117fe48c102c2f6a564f1d033dd80c913
ankur <ankurrathi@gmail.com>
2022-09-19 19:15:59 +0100

    adds test and code for from number 4 to 10

--- src/main/java/com/test/RomanNumeral.java
+++ src/main/java/com/test/RomanNumeral.java
@@ -7,7 +7,14 @@ public class RomanNumeral {
     private static final Map<Integer, String> romanNumeralsMap = Map.of(
             1, "I",
             2, "II",
-            3, "III");
+            3, "III",
+            4, "IV",
+            5, "V",
+            6, "VI",
+            7, "VII",
+            8, "VIII",
+            9, "IX",
+            10, "X");
 
     public String convert(int number) {
         return romanNumeralsMap.get(number);
--- src/test/java/com/test/RomanNumeralTest.java
+++ src/test/java/com/test/RomanNumeralTest.java
@@ -28,4 +28,39 @@ class RomanNumeralTest {
     void shouldReturnRomanNumeralOf3() {
         assertEquals("III", romanNumeral.convert(3));
     }
+
+    @Test
+    void shouldReturnRomanNumeralOf4() {
+        assertEquals("IV", romanNumeral.convert(4));
+    }
+
+    @Test
+    void shouldReturnRomanNumeralOf5() {
+        assertEquals("V", romanNumeral.convert(5));
+    }
+
+    @Test
+    void shouldReturnRomanNumeralOf6() {
+        assertEquals("VI", romanNumeral.convert(6));
+    }
+
+    @Test
+    void shouldReturnRomanNumeralOf7() {
+        assertEquals("VII", romanNumeral.convert(7));
+    }
+
+    @Test
+    void shouldReturnRomanNumeralOf8() {
+        assertEquals("VIII", romanNumeral.convert(8));
+    }
+
+    @Test
+    void shouldReturnRomanNumeralOf9() {
+        assertEquals("IX", romanNumeral.convert(9));
+    }
+
+    @Test
+    void shouldReturnRomanNumeralOf10() {
+        assertEquals("X", romanNumeral.convert(10));
+    }
 }
