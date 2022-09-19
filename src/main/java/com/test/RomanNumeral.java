3c110bd67dd92f58c277718b3d7006df657ac6e2
ankur <ankurrathi@gmail.com>
2022-09-19 18:49:32 +0100

    adds failing test for number 1

---
+++ src/main/java/com/test/RomanNumeral.java
@@ -0,0 +1,7 @@
+package com.test;
+
+public class RomanNumeral {
+    public String convert(int i) {
+        return null;
+    }
+}
--- src/test/java/com/test/RomanNumeralTest.java
+++ src/test/java/com/test/RomanNumeralTest.java
@@ -1,13 +1,22 @@
 package com.test;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RomanNumeralTest {
 
+    private RomanNumeral romanNumeral;
+
+    @BeforeEach
+    void setUp() {
+        romanNumeral = new RomanNumeral();
+    }
+
     @Test
-    void test() {
-        assertTrue(true);
+    void shouldReturnRomanNumeralOf1() {
+        assertEquals("I", romanNumeral.convert(1));
     }
 }
