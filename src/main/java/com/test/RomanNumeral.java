f3bf89596712c3f8940cbb33ce8896ce319043f8
ankur <ankurrathi@gmail.com>
2022-09-19 23:38:07 +0100

    refactoring

--- src/main/java/com/test/RomanNumeral.java
+++ src/main/java/com/test/RomanNumeral.java
@@ -1,34 +1,26 @@
 package com.test;
 
-import java.util.Map;
-
 public class RomanNumeral {
-
-    private static final Map<Integer, String> romanNumeralsMap = Map.of(
-            1, "I",
-            2, "II",
-            3, "III",
-            4, "IV");
 
     public String convert(int number) {
         StringBuilder romanNumeral = new StringBuilder();
-        while (number >= 10) {
-            number = number % 10;
-            romanNumeral.append("X");
-        }
-        while (number >= 9) {
-            number = number % 9;
-            romanNumeral.append("IX");
-        }
-        while (number >= 5) {
-            number = number % 5;
-            romanNumeral.append("V");
-        }
-        if (number > 0) {
-            romanNumeral.append(romanNumeralsMap.get(number));
-        }
+        number = remainder(number, romanNumeral, 10, "X");
+        number = remainder(number, romanNumeral, 9, "IX");
+        number = remainder(number, romanNumeral, 5, "V");
+        number = remainder(number, romanNumeral, 4, "IV");
+        number = remainder(number, romanNumeral, 3, "III");
+        number = remainder(number, romanNumeral, 2, "II");
+        remainder(number, romanNumeral, 1, "I");
 
         return romanNumeral.toString();
+    }
+
+    private int remainder(int number, StringBuilder romanNumeral, int divisor, String romanSymbol) {
+        while (number >= divisor) {
+            number = number % divisor;
+            romanNumeral.append(romanSymbol);
+        }
+        return number;
     }
 
 }
