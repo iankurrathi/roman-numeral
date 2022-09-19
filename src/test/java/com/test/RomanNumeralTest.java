7a88d8deaa43d05c38339703cdb70d6c8fba3f6f
ankur <ankurrathi@gmail.com>
2022-09-19 18:33:42 +0100

    adds junit support

---
+++ src/test/java/com/test/RomanNumeralTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class RomanNumeralTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
